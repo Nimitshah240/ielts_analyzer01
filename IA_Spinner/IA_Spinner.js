524f000cf28bd6f7bc0404d778dda9bb747fb022
Nimit_Shah <nimitshah099@gmail.com>
2024-09-14 11:55:15 +0530

    testing spinner bug issue

--- IA_Spinner/IA_Spinner.js
+++ IA_Spinner/IA_Spinner.js
@@ -1,9 +1,13 @@
 try {
     window.addEventListener("spinner", (event) => {
         if (event.detail.message == 'true') {
+            console.log(event.detail.message);
+
             document.getElementById("spinner").style.display = 'flex';
             document.getElementById("main").style.display = 'none';
         } else if (event.detail.message == 'false') {
+            console.log(event.detail.message);
+
             document.getElementById("spinner").style.display = 'none';
             document.getElementById("main").style.display = 'block';
         }
